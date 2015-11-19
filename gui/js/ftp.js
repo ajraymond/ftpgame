1f1f21bebdcb4533d4ed5b4260979a7f8365f4b5
Eric Demers <eric.demers@gmail.com>
2015-11-18 22:38:40 -0500

    Connect storage and file list. Add upload of new files by drag and drop.

--- gui/js/ftp.js
+++ gui/js/ftp.js
@@ -2,26 +2,35 @@ $(document).ready(function(){
 
     function updateFileLists() {
         var template = $.templates("#fileTemplate");
-        $("#local").html(template.render(localData));
+        $("#local").html(template.render(getAllFiles()));
         $("#remote").html(template.render(remoteData));
+        
         $('tr:not(:has(th))').click(function() {
             $(this).closest("tr").siblings().removeClass("highlighted");
             $(this).toggleClass("highlighted");
         })
     }
       
-    var localData = [
-        { "id": 0, "filename": "file1.txt" },
-        { "id": 1, "filename": "file2.txt" },
-        { "id": 2, "filename": "file3.html" },
-    ];
-
+    
+    
+    
     var remoteData = [];
     for (i=0; i < 200; i++) {
-        remoteData.push( { "id": i, "filename": "file"+i+".txt" })
+        remoteData.push( { "name": i, "contents": "file"+i+".txt" })
     }  
     
     updateFileLists();
+    
+    var opts = {dragClass:"bilboDraggins",
+    on: {
+    load: function(e, file) {
+      addFile(file.name, e.target.result);
+      updateFileLists();
+    }
+  }}
+    $("#dropzone").fileReaderJS(opts);
+    $("body").fileClipboard(opts);
+    
 });
 
 
